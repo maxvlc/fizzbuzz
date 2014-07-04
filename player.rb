882cfded39d7e894854011605f95b21810ec40b9
maxvlc <maxcar@gmail.com>
2014-07-04 19:47:19 +0200

    prints Buzz if number is ten green

--- player.rb
+++ player.rb
@@ -8,11 +8,15 @@ class Player
 	def self.play(number)
 		result = number.to_s
 		result = FIZZ if is_fizz_number?(number)
-		result = BUZZ if number == BUZZ_NUMBER
+		result = BUZZ if is_buzz_number?(number)
 		result
 	end
 
 	def self.is_fizz_number?(number)
 		number%FIZZ_NUMBER == 0
+	end
+
+	def self.is_buzz_number?(number)
+		number%BUZZ_NUMBER == 0
 	end
 end
