6f39dba078aeb99849933b76f829d9aff4b2d254
maxvlc <maxcar@gmail.com>
2014-07-04 18:23:08 +0200

    fizzbuzz player replies Fizz to three refactorized

--- player.rb
+++ player.rb
@@ -1,7 +1,14 @@
 class Player
 
+	FIZZ = "Fizz"
+	FIZZ_NUMBER = 3
+
 	def self.play(number)
-		return "Fizz" if number == 3
+		return FIZZ if is_fizz?(number)
 		number.to_s
+	end
+
+	def self.is_fizz?(number)
+		number == FIZZ_NUMBER
 	end
 end
