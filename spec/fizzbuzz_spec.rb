d657ecf15e2e426fa731f5c434bcc7f8cf31139a
maxvlc <maxcar@gmail.com>
2014-07-04 18:00:15 +0200

    fizzbuzz player replies two to two Red

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,12 @@
 require "./player"
 
 describe "fizzbuzz player" do
+  
   it "replies one to one" do
     expect(Player.play(1)).to eq "1"
+  end
+
+  it "replies two to two" do
+    expect(Player.play(2)).to eq "2"
   end
 end
