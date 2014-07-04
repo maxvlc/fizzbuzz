9e6fea6225c80d9ecb2c034a95de1a91f625b06c
maxvlc <maxcar@gmail.com>
2014-07-04 18:25:57 +0200

    fizzbuzz player replies Buzz to five green

--- player.rb
+++ player.rb
@@ -1,14 +1,21 @@
 class Player
 
 	FIZZ = "Fizz"
+	BUZZ = "Buzz"
 	FIZZ_NUMBER = 3
+	BUZZ_NUMBER = 5
 
 	def self.play(number)
 		return FIZZ if is_fizz?(number)
+		return BUZZ if is_buzz?(number)
 		number.to_s
 	end
 
 	def self.is_fizz?(number)
 		number == FIZZ_NUMBER
+	end
+
+	def self.is_buzz?(number)
+		number == BUZZ_NUMBER
 	end
 end
