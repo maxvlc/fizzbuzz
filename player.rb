8a01e2af2f8c08c755201dc6f2e93fa199b15184
maxvlc <maxcar@gmail.com>
2014-07-04 19:37:14 +0200

    prints Buzz if number is five green

--- player.rb
+++ player.rb
@@ -1,11 +1,14 @@
 class Player
 
 	FIZZ_NUMBER = 3
+	BUZZ_NUMBER = 5
 	FIZZ = "Fizz"
+	BUZZ = "Buzz"
 
 	def self.play(number)
 		result = number.to_s
 		result = FIZZ if number == FIZZ_NUMBER
+		result = BUZZ if number == BUZZ_NUMBER
 		result
 	end
 end
