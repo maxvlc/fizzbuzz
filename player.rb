7c261de903854311f52c13186b5c8571a021e0a2
maxvlc <maxcar@gmail.com>
2014-07-04 19:42:43 +0200

    prints Fizz if number is six refactorized

--- player.rb
+++ player.rb
@@ -7,9 +7,12 @@ class Player
 
 	def self.play(number)
 		result = number.to_s
-		result = FIZZ if number == FIZZ_NUMBER
-		result = FIZZ if number == 6
+		result = FIZZ if is_fizz_number?(number)
 		result = BUZZ if number == BUZZ_NUMBER
 		result
+	end
+
+	def self.is_fizz_number?(number)
+		number%FIZZ_NUMBER == 0
 	end
 end
