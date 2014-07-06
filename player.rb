6d8ac2749d4de9a83453c6e36eb0fa001f86a70b
maxvlc <maxcar@gmail.com>
2014-07-06 17:43:48 +0200

    print buzz if number is five green

--- player.rb
+++ player.rb
@@ -1,11 +1,14 @@
 class Player
 
 	FIZZ = "Fizz"
+	BUZZ = "Buzz"
 	FIZZ_NUMBER = 3
+	BUZZ_NUMBER = 5
 
 	def self.play(number)
 		result = number.to_s
 		result = FIZZ if number == FIZZ_NUMBER
+		result = BUZZ if number == BUZZ_NUMBER
 		result
 	end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,20 +2,18 @@ require "./player"
 
   describe "fizzbuzz player" do
 
-  it "print one if number is one" do
-    expect(Player.play(1)).to eq "1"    
-  end
-
-  it "print two if number is two" do
-    expect(Player.play(2)).to eq "2"    
+  it "print number" do
+    expect(Player.play(1)).to eq "1"
+    expect(Player.play(2)).to eq "2"
+    expect(Player.play(4)).to eq "4"
   end
 
   it "print Fizz if number is three" do
     expect(Player.play(3)).to eq "Fizz"    
   end  
 
-  it "print four if number is four" do
-    expect(Player.play(4)).to eq "4"    
+  it "print Buzz if number is five" do
+    expect(Player.play(5)).to eq "Buzz"    
   end  
 
 end
