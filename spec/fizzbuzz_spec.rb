9d8217f29fa1dfb2e4dd590e935637fe99c17c4a
maxvlc <maxcar@gmail.com>
2014-07-06 17:33:05 +0200

    print one if number is one green

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -5,4 +5,9 @@ require "./player"
   it "print one if number is one" do
     expect(Player.play(1)).to eq "1"    
   end
+
+  it "print two if number is two" do
+    expect(Player.play(2)).to eq "2"    
+  end
+
 end
